2905d228bb766ae81c0b010724b3b78d282533d3
Mateusz <mateuszek1029@gmail.com>
2021-04-09 10:37:17 +0200

    Kolokwium

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let nav = '<h1>Nawigacja</h1> <nav><li><a href="index.html">Strona główna</a></li> ' +
+    ' <li><a href="kontakt.html">Strona niedziałająca</a></li></nav>';
+
+let navbarElem = document.querySelector(".nav-bar");
+
+navbarElem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,4 @@
+let button = document.querySelector("#button");
+let box = document.querySelector('.box')
+
+button.addEventListener('click', () => box.style.backgroundColor='#c9302c')
